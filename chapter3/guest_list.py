b9961f8ffe6eae4a84154ad6d9861361f81d378d
mihir <mihirkumarmishra@gmail.com>
2017-09-17 11:15:38 -0500

    Chapter3: Makes invitation list and print it.

---
+++ chapter3/guest_list.py
@@ -0,0 +1,5 @@
+guest_list = ['Nikhil', 'Sumit Jee', 'Ekta Jee']
+message = ', You are invited for dinner by Rina & Mihir!'
+print('Hi ' + guest_list[0] + message)
+print('Hi ' + guest_list[1] + message)
+print('Hi ' + guest_list[2] + message)

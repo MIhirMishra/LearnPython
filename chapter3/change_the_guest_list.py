42f3319767d753a9efa1e4282932a19b2802a9a5
mihir <mihirkumarmishra@gmail.com>
2017-09-17 11:15:58 -0500

    Chapter3: Changes the invitation list and print it.

---
+++ chapter3/change_the_guest_list.py
@@ -0,0 +1,14 @@
+guest_list = ['Nikhil', 'Sumit Jee', 'Ekta Jee']
+message = ', You are invited for dinner by Rina & Mihir!'
+print('\nHi ' + guest_list[0] + message)
+print('\nHi ' + guest_list[1] + message)
+print('\nHi ' + guest_list[2] + message)
+
+print('\n' +guest_list[0] + ' and ' + guest_list[1] + ' can not make to the dinner.')
+
+guest_list[0] = 'Amit Pandey'
+guest_list[1] = 'Vijayan'
+
+print('\nHi ' + guest_list[0] + message)
+print('\nHi ' + guest_list[1] + message)
+print('\nHi ' + guest_list[2] + message)

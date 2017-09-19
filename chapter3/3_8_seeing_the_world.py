98676237af5a4ac1af3f7ffd6f270819e1228e5e
mihir <mihirkumarmishra@gmail.com>
2017-09-18 20:46:08 -0500

    Chapter3: Rename the file.

---
+++ chapter3/3_8_seeing_the_world.py
@@ -0,0 +1,6 @@
+places_to_visit = ['London', 'Paris', 'Sri Lanka', 'Bhutan', 'Nepal']
+print(places_to_visit)
+print(sorted(places_to_visit))
+print(places_to_visit)
+places_to_visit_reverse = places_to_visit.reverse()
+print(places_to_visit)

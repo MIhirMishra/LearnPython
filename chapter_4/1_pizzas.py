09994c49cc04650dca166f3ce98db428cb49163b
mihir <mihirkumarmishra@gmail.com>
2017-09-19 07:10:56 -0500

    Chapter4: print three lines outside the for loop.

--- chapter_4/1_pizzas.py
+++ chapter_4/1_pizzas.py
@@ -1,3 +1,7 @@
 pizzas = ['cheese pizza', 'thin crust veggie', 'thin crust chicken']
 for pizza in pizzas:
-    print(pizza)
+    print('I like ' + pizza)
+
+print('\nI generally like any thin crust pizza with lot of pineapple on it.'
+      '\nIf garlic flavored cheese is added, it makes the taste still better.'
+      '\nI really love pizza')

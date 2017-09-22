5f8535932489514bbbb76652950a302b1f4c480e
mihir <mihirkumarmishra@gmail.com>
2017-09-21 21:12:32 -0500

    Chapter4: print count to 20.

---
+++ chapter_4/2_animals.py
@@ -0,0 +1,6 @@
+pets = ['cat', 'dog', 'parrot']
+for pet in pets:
+    print('A '+ str(pet) + 'would make a great pet')
+
+
+print('\nall of these pets look very cute')
---
+++ chapter_4/3_counting_to_20.py
@@ -0,0 +1,3 @@
+count_to_twenty = range(1, 21)
+for count in count_to_twenty:
+    print(count)

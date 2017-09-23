a45fc5ac5cf329de5a22d8ca0461bd3d97463b37
mihir <mihirkumarmishra@gmail.com>
2017-09-22 21:27:33 -0500

    Chapter4: slice the list from beginning, middle and last.

---
+++ chapter_4/910_slices.py
@@ -0,0 +1,4 @@
+pets = ['cat', 'dog', 'parrot', 'elephant', 'horse', 'rabbit']
+print('The first three items in the list are:\n' + str(pets[:3]))
+print('The three items from the middle of the list are:\n' + str(pets[2:5]))
+print('The three items from the middle of the list are:\n' + str(pets[3:]))

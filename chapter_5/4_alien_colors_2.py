56a18bd057b2901d04a3b38d1a04a185b6eacee0
mihir <mihirkumarmishra@gmail.com>
2017-09-28 07:03:11 -0500

    Chapter5: usage of if and else.

---
+++ chapter_5/4_alien_colors_2.py
@@ -0,0 +1,6 @@
+alien_colors = ['green', 'yellow', 'red']
+
+if 'green' in alien_colors:
+    print("the player just earned 5 points")
+else:
+    print("the player just earned 10 points")
---
+++ chapter_5/4_alien_colors_2_1.py
@@ -0,0 +1,6 @@
+alien_colors = ['green', 'yellow', 'red']
+
+if 'purple' in alien_colors:
+    print("the player just earned 5 points")
+else:
+    print("the player just earned 10 points")

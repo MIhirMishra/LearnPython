a7c58a9fd25818965777f9d32e1d0625fe760f1f
mihir <mihirkumarmishra@gmail.com>
2017-09-28 07:12:56 -0500

    Chapter5: usage of if and elif.

---
+++ chapter_5/5_alien_colors.py
@@ -0,0 +1,25 @@
+alien_colors = ['green', 'yellow', 'red']
+color = 'green'
+if color == 'green':
+    print("the player just earned 5 points")
+if color == 'yellow':
+    print("the player just earned 10 points")
+if color == 'green':
+    print("the player just earned 15 points")
+
+color = 'yellow'
+if color == 'green':
+    print("the player just earned 5 points")
+elif color == 'yellow':
+    print("the player just earned 10 points")
+elif color == 'red':
+    print("the player just earned 15 points")
+
+
+color = 'red'
+if color == 'green':
+    print("the player just earned 5 points")
+elif color == 'yellow':
+    print("the player just earned 10 points")
+elif color == 'red':
+    print("the player just earned 15 points")
